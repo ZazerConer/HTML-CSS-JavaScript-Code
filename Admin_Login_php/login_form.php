5604fc2c111d7136328cce373d9ad8d73041a04d
Zazer Coner's <96803085+ZazerConer@users.noreply.github.com>
2022-07-01 21:52:40 +0800

    Update login_form.php

--- Admin_Login_php/login_form.php
+++ Admin_Login_php/login_form.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login form</title>
+   <title>Login-Form</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
